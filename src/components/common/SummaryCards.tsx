4c69462ed01b059fd642799a3c6be45ca732164f
Peadar Byrne <peadarjb@gmail.com>
2025-07-26 21:56:29 +0100

    Spacing between summary cards

--- src/components/common/SummaryCards.tsx
+++ src/components/common/SummaryCards.tsx
@@ -1,20 +1,15 @@
-// src/components/common/SummaryCards.tsx
-// FIXED: Optimized selectors to prevent infinite loops
-
 import React from 'react';
 import { Row, Col, Card, Statistic, Select } from 'antd';
 import { EuroCircleOutlined, AreaChartOutlined } from '@ant-design/icons';
 import { usePavementStore } from '../../store/usePavementStore';
 
 const SummaryCards: React.FC = () => {
-  // FIXED: Use individual selectors instead of object selectors to prevent infinite loops
   const totalCost = usePavementStore((state) => state.totalCost);
   const totalLength = usePavementStore((state) => state.totalLength);
   const selectedCounty = usePavementStore((state) => state.selectedCounty);
   const availableCounties = usePavementStore((state) => state.availableCounties);
   const setSelectedCounty = usePavementStore((state) => state.setSelectedCounty);
 
-  // Format total cost for display
   const formatCost = (cost: number): { value: number; suffix: string } => {
     if (cost >= 1e9) {
       return { value: cost / 1e9, suffix: 'B' };
@@ -28,15 +23,14 @@ const SummaryCards: React.FC = () => {
 
   const formattedCost = formatCost(totalCost);
 
-  // Create options for the county selector
   const countyOptions = [
     { value: 'all', label: 'Local Authority (All)' },
     ...availableCounties.map(county => ({ value: county, label: county }))
   ];
 
   return (
-    <Row gutter={[16, 16]} align="middle" justify="space-between" style={{ width: '100%' }}>
-      <Col xs={24} sm={12} md={8} lg={6}>
+    <Row gutter={[24, 24]} align="middle">
+      <Col xs={24} sm={12} md={8}>
         <Card>
           <Statistic
             title="Total Cost"
@@ -48,7 +42,7 @@ const SummaryCards: React.FC = () => {
           />
         </Card>
       </Col>
-      <Col xs={24} sm={12} md={8} lg={6}>
+      <Col xs={24} sm={12} md={8}>
         <Card>
           <Statistic
             title="Total Length"
@@ -60,7 +54,7 @@ const SummaryCards: React.FC = () => {
           />
         </Card>
       </Col>
-      <Col xs={24} sm={24} md={8} lg={6} style={{ textAlign: 'right' }}>
+      <Col xs={24} sm={24} md={8}>
         <Select
           value={selectedCounty}
           style={{ width: '100%' }}
