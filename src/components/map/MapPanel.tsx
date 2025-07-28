b27a40ae0ad9885c4ae964c60121204cb9ee3120
Peadar Byrne <peadarjb@gmail.com>
2025-07-28 11:32:09 +0100

    Map panel update

--- src/components/map/MapPanel.tsx
+++ src/components/map/MapPanel.tsx
@@ -1,14 +1,23 @@
 // src/components/map/MapPanel.tsx
 import React from 'react';
 import { Card } from 'antd';
+import { usePavementStore } from '../../store/usePavementStore';
 import MapComponent from './MapComponent';
 
 const MapPanel: React.FC = () => {
+  const isMobileView = usePavementStore((state) => state.isMobileView);
+  
   return (
     <Card 
       title="Road Network Map" 
+      size={isMobileView ? 'small' : 'default'}
       style={{ height: '100%' }}
-      styles={{ body: { height: 'calc(100% - 57px)', padding: '12px' } }}
+      styles={{ 
+        body: { 
+          height: isMobileView ? 'calc(100% - 38px)' : 'calc(100% - 57px)', 
+          padding: isMobileView ? '8px' : '12px' 
+        } 
+      }}
     >
       <MapComponent />
     </Card>
