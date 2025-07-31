ab03f5873a7df84953a5a65cee62be1b1f3a5483
Peadar Byrne <peadarjb@gmail.com>
2025-07-31 17:26:04 +0100

    Changing menu for mobile

--- src/components/controls/ParametersDrawer.tsx
+++ src/components/controls/ParametersDrawer.tsx
@@ -1,12 +1,13 @@
 // src/components/controls/ParametersDrawer.tsx
 import React from 'react';
-import { Drawer, Collapse, Button, Form } from 'antd';
+import { Drawer, Collapse, Button, Form, Space, Typography } from 'antd';
 import { CloseOutlined, ReloadOutlined } from '@ant-design/icons';
 import { usePavementStore } from '../../store/usePavementStore';
 import ParametersSection from './ParametersSection';
 import CostsSection from './CostsSection';
 
 const { Panel } = Collapse;
+const { Title } = Typography;
 
 const ParametersDrawer: React.FC = () => {
   const isOpen = usePavementStore((state) => state.isParameterDrawerOpen);
@@ -14,18 +15,48 @@ const ParametersDrawer: React.FC = () => {
   const resetParameters = usePavementStore((state) => state.resetParameters);
   const resetCosts = usePavementStore((state) => state.resetCosts);
 
+  // Custom title with logos and text
+  const drawerTitle = (
+    <Space direction="vertical" align="center" style={{ width: '100%' }}>
+      <Space size="middle" style={{ marginBottom: 8 }}>
+        <img 
+          src="/img/dtts-logo.png" 
+          alt="DTTS" 
+          style={{ height: 24 }}
+        />
+        <img 
+          src="/img/rmo-logo.png" 
+          alt="RMO" 
+          style={{ height: 24 }}
+        />
+        <img 
+          src="/img/pvs-logo.png" 
+          alt="PVS" 
+          style={{ height: 24 }}
+        />
+      </Space>
+      <Title level={5} style={{ margin: 0 }}>
+        RMO Dashboard
+      </Title>
+    </Space>
+  );
+
   return (
     <Drawer
-      title="Parameters & Costs"
+      title={drawerTitle}
       placement="left"
       onClose={() => setParameterDrawerOpen(false)}
       open={isOpen}
       width="85%"
       styles={{
-        body: { paddingTop: 0 }
+        body: { paddingTop: 16 },
+        header: { paddingBottom: 16 }
       }}
       closeIcon={<CloseOutlined />}
     >
+      <Title level={5} style={{ marginBottom: 16 }}>
+        Parameters & Costs
+      </Title>
       <Form layout="horizontal" colon={false}>
         <Collapse 
           defaultActiveKey={['parameters', 'costs']}
